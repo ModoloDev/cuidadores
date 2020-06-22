4ef9d4b340d4e47858db486fd4660fb03af99761
Rodrigo <rodrigojipe@hotmail.com>
2020-06-22 16:24:15 -0300

    cadastro e api 2

---
+++ js/cadastro.js
@@ -0,0 +1,12 @@
+
+document.getElementById('btnCadastro').addEventListener('click', () => {
+
+    const nome = "Teste1"
+    const cpf = document.getElementById('inputCPF').value;
+    const email = document.getElementById('inputEmail').value;
+    const senha = document.getElementById('inputSenha').value;
+    const repetirSenha = document.getElementById('inputRepetirSenha').value;
+    const checkmarkPaciente = document.getElementById('checkmarkPaciente').checked;
+    const checkmarkCuidador = document.getElementById('checkmarkCuidador').checked;
+
+});
