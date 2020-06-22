07fbb97bb4966d97f5870dc8797db82e5945ac5f
Lucas Módolo <lucasmg2001@gmail.com>
2020-06-22 16:47:54 -0300

    atualização das telas

---
+++ js/login.js
@@ -0,0 +1,18 @@
+//Button Function
+// $(".fa-bars").click(function() {
+//     if (idmySidenav.width() == 0){
+//         $('.main').animate({left: '0'}, 'slow');
+//         idmySidenav.width('49%');
+//         idmySidenav.show(200);
+//         classtextmysidenax.show();
+//         // idopencloseli.animate.css('textShadow', '0 0 15px rgba(244, 0, 0, 0.71)');
+//         idsaberbutton.removeClass('fa fa-arrow-circle-right').addClass('fa fa-arrow-circle-left');
+//     }
+//     else{
+//         $('.main').animate({left: '25%'}, 'slow');
+//         classtextmysidenax.hide("fast");
+//         idmySidenav.width('0%');
+//         idsaberbutton.removeClass('fa fa-arrow-circle-left').addClass('fa fa-arrow-circle-right');
+//         idmySidenav.fadeOut("100");
+//     }
+// });
