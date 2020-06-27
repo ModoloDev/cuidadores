ca6bbd1c41bc8255089682927b617af5ede96720
Rodrigo <rodrigojipe@hotmail.com>
2020-06-27 03:47:18 -0300

    teste cookie

--- js/login.js
+++ js/login.js
@@ -1,22 +1,3 @@
-//Button Function
-// $(".fa-bars").click(function() {
-//     if (idmySidenav.width() == 0){
-//         $('.main').animate({left: '0'}, 'slow');
-//         idmySidenav.width('49%');
-//         idmySidenav.show(200);
-//         classtextmysidenax.show();
-//         // idopencloseli.animate.css('textShadow', '0 0 15px rgba(244, 0, 0, 0.71)');
-//         idsaberbutton.removeClass('fa fa-arrow-circle-right').addClass('fa fa-arrow-circle-left');
-//     }
-//     else{
-//         $('.main').animate({left: '25%'}, 'slow');
-//         classtextmysidenax.hide("fast");
-//         idmySidenav.width('0%');
-//         idsaberbutton.removeClass('fa fa-arrow-circle-left').addClass('fa fa-arrow-circle-right');
-//         idmySidenav.fadeOut("100");
-//     }
-// });
-
 const URL_API = 'https://trabalhomodolo.rj.r.appspot.com'
 
 document.getElementById('entrar-id').addEventListener('click', async () => {
