74674690f94aac9d060d20f4156a4e303182b901
Rodrigo <rodrigojipe@hotmail.com>
2020-06-27 00:53:59 -0300

    teste cookie 9

--- js/loginCuidador.js
+++ js/loginCuidador.js
@@ -1,5 +1,5 @@
 console.log(document.cookie)
-var userInfo = document.cookie.split(';')[1]
+var userInfo = document.cookie
 console.log(userInfo);
 userInfo = atob(userInfo.split('=')[1])
 console.log(userInfo)
