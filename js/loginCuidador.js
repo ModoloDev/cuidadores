ae45822712a24afedcd4746eb667b1e54941468a
Rodrigo <rodrigojipe@hotmail.com>
2020-06-27 01:03:17 -0300

    teste cookie

--- js/loginCuidador.js
+++ js/loginCuidador.js
@@ -3,6 +3,5 @@ var userInfo = document.cookie
 console.log(userInfo);
 userInfo = atob(userInfo.split('=')[1])
 userInfoStr = JSON.stringify(userInfo)
-console.log(userInfoStr)
-userInfoParse = JSON.parse(userInfo)
-console.log(userInfoParse)
+console.log(userInfoStr.email)
+console.log(userInfo.email)
