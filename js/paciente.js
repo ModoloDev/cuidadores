b638e836b9026bfc4cc8268f2f45b8f796b0a02b
Rodrigo <rodrigojipe@hotmail.com>
2020-06-28 04:23:48 -0300

    infos pacientes

--- js/paciente.js
+++ js/paciente.js
@@ -11,5 +11,47 @@ $.extend({
     }
 })
 
-var cpfPaciente = $.getUrlVars()['cpf'];
-console.log(cpfPaciente)
+var cpfPaciente = $.getUrlVars()['c'];
+cpfPaciente = atob(cpfPaciente);
+
+infoPaciente = async (cpfPaciente) => {
+
+    let payloadJSON = JSON.stringify({cpf: cpfPaciente})
+
+    var paciente = await fetch(`${URL_API}/user/cpf`, {
+        method: 'POST',
+        body: payloadJSON,
+        headers: {"Content-Type": "application/json; charset=UTF-8"}
+    });
+    await paciente.json().then((data) => {
+        document.getElementById('outputNomePac').innerHTML = data.data.paciente.nome;
+        document.getElementById('outputSexoPac').innerHTML = data.data.paciente.sexo;
+        document.getElementById('outputEnderecoPac').innerHTML = data.data.paciente.endereco;
+        document.getElementById('outputDataNscPac').innerHTML = data.data.paciente.dataNsc;
+    })
+}
+
+getCuidador = async (cpfPaciente) => {
+
+    var cuidadores = await fetch(`${URL_API}/cuidadores`, {
+        method: 'POST',
+        body: {}
+        // headers: {"Content-Type": "application/json; charset=UTF-8"}
+    });
+    await cuidadores.json().then((data) => {
+        for (var cuidador in data.data) {
+            for (var listPaciente in data.data[cuidador].pacientes) {
+                if (data.data[cuidador].pacientes[listPaciente] == cpfPaciente) {
+                    document.getElementById('outputNomeCuid').innerHTML = data.data[cuidador].nome;
+                    document.getElementById('outputDataNscCuid').innerHTML = data.data[cuidador].dataNsc;
+                    document.getElementById('outputGeneroCuid').innerHTML = data.data[cuidador].genero;
+                    document.getElementById('outputTelefoneCuid').innerHTML = data.data[cuidador].telefone;
+                    return;
+                }
+            }
+        }
+    })
+}
+
+infoPaciente(cpfPaciente);
+getCuidador(cpfPaciente);
--- js/showPacientes.js
+++ js/showPacientes.js
@@ -1,4 +1,7 @@
 listPacientes = async () => {
+
+    var user = document.getElementsByClassName('identificacao')[0].id;
+
     var payloadJSON = JSON.stringify({cpf: userInfo.cpf});
 
     var pacientes = await fetch(`${URL_API}/user/cpf`, {
@@ -8,14 +11,14 @@ listPacientes = async () => {
     })
     await pacientes.json().then(async (data) => {
         if (data.data === undefined) {return;}
-        if (Object.keys(data.data.responsavel).length == 0){
+        if (Object.keys(data.data[user]).length == 0){
             window.location.href = 'https://lucasmodolo22.github.io/cuidadores';
         } else {
-            if ((data.data.responsavel.pacientes).length == 0) {
-                document.getElementById('listPacientes').innerHTML = 'Sem pacientes cadastrados!';
+            if ((data.data[user].pacientes).length == 0) {
+                document.getElementById('listPaciente').innerHTML = '<div class="login"><i class="paciente"><ul><h3>Sem pacientes cadastrados</h3></ul></i></div></a>';
             } else {
-                for (var paciente in data.data.responsavel.pacientes) {
-                    var payloadJSONPaciente = JSON.stringify({cpf: data.data.responsavel.pacientes[paciente]});
+                for (var paciente in data.data[user].pacientes) {
+                    var payloadJSONPaciente = JSON.stringify({cpf: data.data[user].pacientes[paciente]});
 
                     var listaPacientes = await fetch(`${URL_API}/user/cpf`, {
                         method: 'POST',
@@ -23,7 +26,8 @@ listPacientes = async () => {
                         headers: {"Content-Type": "application/json; charset=UTF-8"}
                     })
                     await listaPacientes.json().then(async (response) => {
-                        document.getElementById('listPaciente').innerHTML += `<a href="paciente.html?cpf=${data.data.responsavel.pacientes[paciente]}"><div class="login"><i class="paciente"><ul><i class="fa fa-user"></i><h3>${response.data.paciente.nome}</h3></ul></i></div></a>`
+                        var cpfEncoded = btoa(data.data[user].pacientes[paciente])
+                        document.getElementById('listPaciente').innerHTML += `<a href="paciente.html?c=${cpfEncoded}"><div class="login"><i class="paciente"><ul><i class="fa fa-user"></i><h3>${response.data.paciente.nome}</h3></ul></i></div></a>`
                     })
                 }
             }
