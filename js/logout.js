cb8994a49bfc175b40448eb9f16b7897aebf9b37
Rodrigo <rodrigojipe@hotmail.com>
2020-06-29 00:33:26 -0300

    o maior commit de todos os tempos

--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,4 @@
 document.getElementById('btnLogout').addEventListener('click', () => {
-    document.cookie = 'user=undefined';
+    document.cookie = 'user=undefined;';
     window.location.href = 'index.html';
 })
