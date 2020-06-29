9c9fc17fefe53910ff9c1162efa7d2745529b092
Rodrigo <rodrigojipe@hotmail.com>
2020-06-29 00:39:07 -0300

    teste cookie

--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,4 @@
 document.getElementById('btnLogout').addEventListener('click', () => {
-    document.cookie = 'user=;';
+    document.cookie = 'user=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
     window.location.href = 'index.html';
 })
