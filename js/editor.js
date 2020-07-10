e341061ebb0267a59a6ad188b9b7ce2a49061f11
Rodrigo <rodrigojipe@hotmail.com>
2020-07-09 23:25:48 -0300

    saving info paciente

--- js/editor.js
+++ js/editor.js
@@ -1,13 +1,79 @@
-const btnSave = document.getElementById('btnSave').addEventListener('click', () => {
-    editor.save().then(data => {
+getInfoPaciente = async () => {
+    return new Promise(async (resolve) => {
 
-        payloadJSON = JSON.stringify(data)
-        console.log(payloadJSON)
+        var payloadJSON = JSON.stringify({cpf: cpfPaciente});
 
-        fetch(`${URL_API}/save/info`, {
+        var result = await fetch(`${URL_API}/user/cpf`, {
             method: 'POST',
             body: payloadJSON,
             headers: {"Content-Type": "application/json; charset=UTF-8"}
+        });
+        await result.json().then((data) => {
+            resolve(data.data.paciente[0].info)
         })
     })
-})
+};
+
+showInfo = async () => {
+    var savedData = await getInfoPaciente();
+
+    const editor = new EditorJS({
+        holder: 'editorjs',
+        tools: {
+
+            header: {
+            class: Header,
+            inlineToolbar: ['link'],
+            config: {
+                placeholder: 'Header'
+            }
+            },
+
+            image: {
+            class: SimpleImage,
+            inlineToolbar: ['link'],
+            },
+
+            list: {
+            class: List,
+            inlineToolbar: true
+            },
+
+            checklist: {
+            class: Checklist,
+            inlineToolbar: true,
+            },
+
+            delimiter: Delimiter,
+            inlineCode: {
+            class: InlineCode
+            },
+
+            linkTool: LinkTool,
+            embed: Embed,
+            table: {
+            class: Table,
+            inlineToolbar: true
+            }
+        },
+        data: savedData
+    });
+
+    document.getElementById('btnSave').addEventListener('click', async () => {
+        editor.save().then(async data => {
+
+            payloadJSON = JSON.stringify({
+                cpf: cpfPaciente,
+                info: data
+            })
+            console.log(payloadJSON)
+
+            await fetch(`${URL_API}/save/info`, {
+                method: 'POST',
+                body: payloadJSON,
+                headers: {"Content-Type": "application/json; charset=UTF-8"}
+            })
+        })
+    })
+}
+showInfo();
